f0498fd52f84d0b61d8073becbe6ee1699546d36
Yugeta.Koji <geta1972@gmail.com>
2023-12-16 08:55:16 +0900

    options : event

--- extensions/options/js/storage.js
+++ extensions/options/js/storage.js
@@ -4,16 +4,32 @@ import { Element } from "../../contents/js/element.js"
 
 export class Storage{
   constructor(){
-    this.init()
+    this.set_event()
+    this.set_value()
   }
 
-  init(){
-    const data = Data.storage_load()
+  set_event(){
     if(Element.elm_tag){
       Element.elm_tag.addEventListener("change" , this.change_tag.bind(this))
-      if(data && data.tag){
-        Element.elm_tag.value = data.tag
-      }
+    }
+    if(Element.elm_imagesize){
+      Element.elm_imagesize.addEventListener("change" , this.change_tag.bind(this))
+    }
+    if(Element.elm_fontsize){
+      Element.elm_fontsize.addEventListener("change" , this.change_tag.bind(this))
+    }
+  }
+
+  set_value(){
+    const data = Data.storage_load() || {}
+    if(Element.elm_tag && data.tag){
+      Element.elm_tag.value = data.tag
+    }
+    if(Element.elm_imagesize && data.imagesize){
+      Element.elm_imagesize.value = data.imagesize
+    }
+    if(Element.elm_fontsize && data.fontsize){
+      Element.elm_fontsize.value = data.fontsize
     }
   }
 
